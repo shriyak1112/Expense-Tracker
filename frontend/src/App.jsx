3cd748d2f5ac7bb0df5d58845ae0fa674afb3df8
shriyak1112 <shriyak1112@gmail.com>
2024-07-03 19:01:09 +0530

    completed ui design

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import './index.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
+import SignUpPage from './pages/SignUpPage';
+import TransactionPage from './pages/TransactionPage';
+import Header from './components/ui/Header';
+
+
+
+function App() {
+  const authUser = true;
+  return (
+    <>
+    {authUser && <Header/>}
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+      <Route path='/login' element={<LoginPage/>} />
+      <Route path='/signup' element={<SignUpPage/>} />
+      <Route path='/transaction/:id' element={<TransactionPage />} />
+      <Route path='*' element={<NotFoundPage/>} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
